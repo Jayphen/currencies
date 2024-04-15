d7c90a5cabd10c5e1b1d5ea481691cf831acc864
Jayphen Simpson <jayphen.simpson@n26.com>
2024-04-15 21:36:17 +0200

    feat: Add tooltip to chart

--- app/components/chart/line-chart/LineChart.tsx
+++ app/components/chart/line-chart/LineChart.tsx
@@ -2,13 +2,17 @@ import {
   AnimatedAxis,
   AnimatedGrid,
   AnimatedLineSeries,
+  Tooltip,
   XYChart,
 } from "@visx/xychart";
 import { LoaderResponse } from "~/routes/_index";
+import styles from "./style.module.css";
+
+type Rates = LoaderResponse["rates"][number];
 
 const accessors = {
-  xAccessor: (d: LoaderResponse["rates"][number]) => new Date(d.x),
-  yAccessor: (d: LoaderResponse["rates"][number]) => d.y,
+  xAccessor: (d: Rates) => new Date(d.x),
+  yAccessor: (d: Rates) => d.y,
 };
 
 export function LineChart({ data }: { data: LoaderResponse["rates"] }) {
@@ -29,6 +33,30 @@ export function LineChart({ data }: { data: LoaderResponse["rates"] }) {
         data={data}
         {...accessors}
         stroke="var(--chart-line)"
+      />
+      <Tooltip
+        snapTooltipToDatumX
+        snapTooltipToDatumY
+        showVerticalCrosshair
+        showSeriesGlyphs
+        renderTooltip={({ tooltipData }) =>
+          tooltipData && (
+            <div className={styles.tooltip}>
+              <p>
+                {accessors.yAccessor(tooltipData.nearestDatum?.datum as Rates)}
+              </p>
+              <div>
+                {accessors
+                  .xAccessor(tooltipData.nearestDatum?.datum as Rates)
+                  .toLocaleDateString(undefined, {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+              </div>
+            </div>
+          )
+        }
       />
     </XYChart>
   );
